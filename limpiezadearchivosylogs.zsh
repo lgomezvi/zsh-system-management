ef710938bf3c2a8c60bce641fbea0dd722495adc
laura <lausogovi@gmail.com>
2025-05-28 22:12:02 +0100

    Create limpiezadearchivosylogs.zsh
    el codigo en zsh para limpiar logs y archivos mas de 7 dias viejos

---
+++ limpiezadearchivosylogs.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+# limpiezadearchivosylogs.zsh
+
+# simplemente para saber en el terminal que si esta corriendo 
+echo "Empieza la limpieza"
+
+# revisa los directorios mas comunes con archivos temporales y logs 
+for dir in /tmp /var/log /var/tmp; do
+  if [ -d "$dir" ]; then #revisa si si existe el directorio
+    sudo find "$dir" -type f -mtime +7 -exec rm -f {} \; # busca y elimina
+  fi
+done
+
+echo "Limpieza completada."
