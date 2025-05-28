bcd020a34e82230f6370cc55d7b47dbf6f921ddc
laura <lausogovi@gmail.com>
2025-05-28 22:12:37 +0100

    Update limpiezadearchivosylogs.zsh

--- limpiezadearchivosylogs.zsh
+++ limpiezadearchivosylogs.zsh
@@ -7,7 +7,7 @@ echo "Empieza la limpieza"
 # revisa los directorios mas comunes con archivos temporales y logs 
 for dir in /tmp /var/log /var/tmp; do
   if [ -d "$dir" ]; then #revisa si si existe el directorio
-    sudo find "$dir" -type f -mtime +7 -exec rm -f {} \; # busca y elimina
+    sudo find "$dir" -type f -mtime +7 -exec rm -f {} \; # busca y elimina (en este caso basado en los que tengan mas de 7 dias en el sistema)
   fi
 done
 
